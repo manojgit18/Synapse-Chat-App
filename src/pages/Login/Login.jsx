c54b4d518ad42bdcabb662f5ab9a164364f86d52
manojgit18 <manojmaverick96@gmail.com>
2026-01-13 04:36:50 +0530

    synapse

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+// import React, { useState } from 'react'
 import './Login.css'
 import assets from '../../assets/assets'
 
 const Login = () => {
+  //  const [currState, setCurrState] = useState("Sign Up")
+
+
   return (
     <div className='login'>
       <img src={assets.logo_big} alt="" className='logo'/>
+      <form className='login-form'>
+        <h2>Sign Up</h2>
+        <input type="text" placeholder='username' className='form-input' required/>
+        <input type="email" placeholder='Email address' className='form-input' required/>
+        <input type="password" placeholder='password' className='form-input' required/>
+        <button type='submit'>Sign Up</button>
+        <div className='login-term'>
+          <input type="checkbox" />
+          <p>Agree to the terms of use & privacy policy.</p>
+        </div>
+        <div className='login-forgot'>
+          <p className='login-toggle'>Already have an account <span>Click Here</span></p>
+        </div>
+      </form>
     </div>
   )
 }
